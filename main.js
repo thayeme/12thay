1586d342b6ebd9071ab821651867cf83b299a8da
thayeme <161500169+thayeme@users.noreply.github.com>
2025-04-04 14:56:56 -0300

    gyuhfg

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-300-05T00:00:00");
-const tempoObjetivo2 = new Date("2026-300-05T00:00:00");
-const tempoObjetivo3 = new Date("2026-300-30T00:00:00");
-const tempoObjetivo4 = new Date("2026-300-01T00:00:00");
+const tempoObjetivo1 = new Date("2026-05-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-05-05T00:00:00");
+const tempoObjetivo3 = new Date("2026-05-30T00:00:00");
+const tempoObjetivo4 = new Date("2026-05-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
